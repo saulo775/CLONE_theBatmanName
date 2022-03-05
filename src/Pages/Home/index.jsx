65f026e96679eaeda682a07343eacf5fc5329b10
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-03-05 12:44:52 -0300

    Tela do imput

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 
+
 import {
     Container,
-    Box,
-    Teste
 } from "./styles";
 
 export function Home() {
     return (
-        <Container Image={"../batman.png"}>
-            <h1>MMMMMM</h1>
+        <Container >
+            <div>
+                <h1>ENTER YOUR NAME</h1>
+                <input type="text" id="inputName"/>
+            </div>
+
+            <section className="active">
+                <h3></h3>
+                <button>GO</button>
+            </section>
+            
         </Container>
     )
+
+
+    document.querySelector("#inputName").focus()
 }
--- src/Pages/Home/styles.js
+++ src/Pages/Home/styles.js
@@ -1,19 +1,74 @@
 import styled from "styled-components";
+import batman from './batmanLogo.jpeg'
 
 export const Container = styled.main`
+    font-family: 'League Gothic', sans-serif;
     display: flex;
     align-items: center;
     justify-content: center;
+    flex-direction: column;
     width: 100vw;
     height: 100vh;
     background-color: black;
+    text-align: left;
+    /*
     h1 {
-        font-family: 'League Gothic', sans-serif;
         font-size: 300px;
-        background-image: url('https://cdn.atomix.vg/wp-content/uploads/2020/10/New-Project-79.jpg');
+        //background-image: url('https://cdn.atomix.vg/wp-content/uploads/2020/10/New-Project-79.jpg');
+        background-image: url("https://raw.githubusercontent.com/shajidhasan/batname/main/src/assets/batman_logo_bg_1200.jpg");
         -webkit-background-clip: text;
         -webkit-text-fill-color: transparent;
         background-repeat: no-repeat;
         background-size: cover;
+    } */
+
+    h1 {
+        font-size: 2.25rem;
+        font-weight: 700;
+        color: #B91C1B;
+        text-align: left;
+        margin-bottom: 2rem;
+    }
+
+    input {
+        font-family: 'League Gothic', sans-serif;
+        font-weight: 700;
+        font-size: 3rem;
+        min-width: 500px;
+        text-align: center;
+        background-color: black;
+        border: none;
+        color: #B91C1B;
+        border-bottom: 2px solid #B91C1B;
+        padding-bottom: 1rem;
+        text-transform: uppercase;
+        
+        :focus {
+            outline: none;
+        }
+    }
+
+    section.active {
+        display: flex;
+    }
+
+    section {
+        width: 30rem;
+        display: none;
+        text-align: right;
+        justify-content: space-between;
+        align-items: right;
+
+        button {
+            font-family: 'League Gothic', sans-serif;
+            background-color: #B91C1B;
+            border: none;
+            margin-top: 2rem;
+            padding: 0.5rem 2rem;
+            font-weight: 700;
+            font-size: 1.5rem;
+            color: white;
+            border-radius: 4px;
+        }
     }
 `
